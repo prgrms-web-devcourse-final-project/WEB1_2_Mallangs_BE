2610ccc7a2813aab7d316d3d160c6c1aa692efdd
DongWooKim4343 <kd6904@naver.com>
2024-11-18 12:34:05 +0900

    [rename] 이름 변경

--- src/main/java/com/example/mallangs/MallangsApplication.java
+++ src/main/java/com/example/mallangs/MallangsApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.mallangs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MallangsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MallangsApplication.class, args);
     }
 
 }
--- src/test/java/com/example/mallangs/MallangsApplicationTests.java
+++ src/test/java/com/example/mallangs/MallangsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.mallangs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MallangsApplicationTests {
 
     @Test
     void contextLoads() {
