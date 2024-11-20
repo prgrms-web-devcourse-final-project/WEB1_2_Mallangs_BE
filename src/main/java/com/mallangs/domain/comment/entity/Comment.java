ca353ba8b6a5dda7b402d6b111cbf17a87ad3424
HanJae-Jae <gkswowo98@naver.com>
2024-11-21 03:19:55 +0900

    [feat] Comment 댓글 entity 설계

---
+++ src/main/java/com/mallangs/domain/comment/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.mallangs.domain.comment.entity;
+
+import com.mallangs.domain.member.Member;
+import com.mallangs.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="member_id")
+//    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private PostType postType;
+
+    private Long postId;
+
+    private String content;
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+}
