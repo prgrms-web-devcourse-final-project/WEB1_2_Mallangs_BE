5f084137c9c4fdc36397448c838e65ddfda04723
iam52 <tupac52@gmail.com>
2024-11-21 12:24:31 +0900

    [mod] Category명 임시 지정

--- src/main/java/com/mallangs/domain/community/entity/Category.java
+++ src/main/java/com/mallangs/domain/community/entity/Category.java
@@ -1,12 +1,14 @@
 package com.mallangs.domain.community.entity;
 
 public enum Category {
-    COMMUNITY("커뮤니티"),
-    FEED("피드"),
-    PLACE("장소"),
-    RESCUE("구조"),
-    LOST("실종"),
-    SIGHED("발견");
+
+    // 커뮤니티 카테코리명 임시 지정, 이름 나오면 수정 예정
+    CATEGORY1("카테고리1"),
+    CATEGORY2("카테고리2"),
+    CATEGORY3("카테고리3"),
+    CATEGORY4("카테고리4"),
+    CATEGORY5("카테고리5"),
+    CATEGORY6("카테고리6");
 
     private String value;
 
