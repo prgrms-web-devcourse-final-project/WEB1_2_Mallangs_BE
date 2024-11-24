9689d97fc0f26652283d0773a49c3b54345b384b
Dom1046 <rkrkwnj1046@gmail.com>
2024-11-25 04:47:28 +0900

    [feat] customMemberDetail 추가

---
+++ src/main/java/com/mallangs/domain/jwt/entity/CustomMemberDetails.java
@@ -0,0 +1,60 @@
+package com.mallangs.domain.jwt.entity;
+
+import com.mallangs.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class CustomMemberDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add((GrantedAuthority) () -> member.getMemberRole().name());
+        return collection;
+    }
+
+    public String getEmail(){
+        return member.getEmail().getValue();
+    }
+    public String getUserId(){
+        return member.getUserId().getValue();
+    }
+    public String getRole(){return member.getMemberRole().name();}
+
+    @Override
+    public String getPassword() {
+        return member.getPassword().getValue();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getNickname().getValue();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
