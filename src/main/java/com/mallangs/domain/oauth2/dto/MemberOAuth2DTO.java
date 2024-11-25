44f5bf831752fe3a260d8386334f7924cf5ea194
Dom1046 <rkrkwnj1046@gmail.com>
2024-11-25 17:46:30 +0900

    [feat] CustomOAuth2MemberService 추가, 리소스 서버로 부터 값을 받아와서 어플리케이션에서 정제하는 로직

--- src/main/java/com/mallangs/domain/oauth2/dto/MemberOAuth2DTO.java
+++ src/main/java/com/mallangs/domain/oauth2/dto/MemberOAuth2DTO.java
@@ -4,11 +4,26 @@ import lombok.Getter;
 
 @Getter
 public class MemberOAuth2DTO {
-
     private String userId;
     private String password;
     private String nickname;
     private String email;
     private String role;
+
+    public void changeUserId(String userId) {
+        this.userId = userId;
+    }
+    public void changePassword(String password) {
+        this.password = password;
+    }
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public void changeEmail(String email) {
+        this.email = email;
+    }
+    public void changeRole(String role) {
+        this.role = role;
+    }
 
 }
