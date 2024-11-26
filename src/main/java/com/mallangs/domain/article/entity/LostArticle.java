b53663082a00d933920bcb6eccfd6688a3f829ec
username0w <grandcias123@gmail.com>
2024-11-27 02:32:57 +0900

    feat: LostArticle 객체 생성 메서드 추가

--- src/main/java/com/mallangs/domain/article/entity/LostArticle.java
+++ src/main/java/com/mallangs/domain/article/entity/LostArticle.java
@@ -1,5 +1,7 @@
 package com.mallangs.domain.article.entity;
 
+import com.mallangs.domain.article.dto.request.LostCreateRequest;
+import com.mallangs.domain.member.Member;
 import com.mallangs.domain.pet.entity.PetGender;
 import com.mallangs.domain.pet.entity.PetType;
 import jakarta.persistence.Column;
@@ -8,7 +10,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -16,7 +17,6 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @DiscriminatorValue("lost")
 @SuperBuilder
 public class LostArticle extends Article {
@@ -46,6 +46,7 @@ public class LostArticle extends Article {
   @Enumerated(EnumType.STRING)
   @Column(name = "lost_status", nullable = false)
   private LostStatus lostStatus;
+
 
   @Override
   public void applyChanges(Article updatedArticle) {
@@ -84,5 +85,27 @@ public class LostArticle extends Article {
         this.lostStatus = updatedLostArticle.getLostStatus();
       }
     }
+  }
+
+  public static LostArticle createLostArticle(Member member, LostCreateRequest createRequest) {
+    return LostArticle.builder()
+        .petType(createRequest.getPetType())
+        .breed(createRequest.getBreed())
+        .petName(createRequest.getPetName())
+        .petAge(createRequest.getPetAge())
+        .petGender(createRequest.getPetGender())
+        .neutered(createRequest.getNeutered())
+        .chipNumber(createRequest.getChipNumber())
+        .lostDate(createRequest.getLostDate())
+        .lastSeenLocation(createRequest.getLastSeenLocation())
+        .lostStatus(createRequest.getLostStatus())
+        .member(member)
+        .mapVisibility(createRequest.getMapVisibility())
+        .title(createRequest.getTitle())
+        .geography(createRequest.getGeography())
+        .description(createRequest.getDescription())
+        .contact(createRequest.getContact())
+        .image(createRequest.getImage())
+        .build();
   }
 }
