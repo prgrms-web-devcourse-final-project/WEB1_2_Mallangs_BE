6be865ae9d143312756d56d6b3a50e2bc3e78aba
username0w <grandcias123@gmail.com>
2024-11-27 02:23:09 +0900

    feat: Article 위치 데이터 타입 변경

--- src/main/java/com/mallangs/domain/article/entity/Article.java
+++ src/main/java/com/mallangs/domain/article/entity/Article.java
@@ -1,20 +1,25 @@
 package com.mallangs.domain.article.entity;
 
+import com.mallangs.domain.member.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -22,7 +27,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "article_type")
+@DiscriminatorColumn(name = "article_type") // db 에 존재
 @Getter
 @NoArgsConstructor
 @SuperBuilder
@@ -39,13 +44,16 @@ public abstract class Article {
   private MapVisibility mapVisibility;
 
   @Column(nullable = false, length = 100)
-  private String title;
+  private String title; // 장소인 경우 장소 이름
 
-  @Column(nullable = false)
-  private Double latitude;
+//  @Column(nullable = false)
+//  private Double latitude;
+//
+//  @Column(nullable = false)
+//  private Double longitude;
 
-  @Column(nullable = false)
-  private Double longitude;
+  @Column(nullable = false, columnDefinition = "POINT SRID 4326")
+  private Point geography;
 
   @Column(length = 500)
   private String description;
@@ -55,9 +63,9 @@ public abstract class Article {
   @Column(columnDefinition = "TEXT")
   private String image;
 
-//  @ManyToOne(fetch = FetchType.LAZY)
-//  @JoinColumn(name = "member_id")
-//  private Member member;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
 
   @CreatedDate
   @Column(name = "created_at", nullable = false, updatable = false)
@@ -69,6 +77,9 @@ public abstract class Article {
 
 
   public void applyChanges(Article updatedArticle) {
+    if (updatedArticle.getMapVisibility() != null) {
+      this.mapVisibility = updatedArticle.getMapVisibility();
+    }
     if (updatedArticle.getTitle() != null) {
       this.title = updatedArticle.getTitle();
     }
