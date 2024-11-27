0aaafef0feb621944b4baf1dcd230f66e152feda
username0w <grandcias123@gmail.com>
2024-11-27 11:54:32 +0900

    chore: ErrorCode 추가

--- src/main/java/com/mallangs/global/exception/ErrorCode.java
+++ src/main/java/com/mallangs/global/exception/ErrorCode.java
@@ -14,7 +14,11 @@ public enum ErrorCode {
   MEMBER_NOT_FOUND(NOT_FOUND, "회원 정보를 찾을 수 없습니다."),
 
   // Address
-  ADDRESS_NOT_FOUND(NOT_FOUND, "주소 정보를 찾을 수 없습니다.");
+  ADDRESS_NOT_FOUND(NOT_FOUND, "주소 정보를 찾을 수 없습니다."),
+
+  // Article
+  ARTICLE_TYPE_NOT_FOUND(NOT_FOUND, "해당하는 글타래 타입을 찾을 수 없습니다."),
+  ARTICLE_NOT_FOUND(NOT_FOUND, "글타래 정보를 찾을 수 없습니다.");
 
 
   private final HttpStatus httpStatus;
