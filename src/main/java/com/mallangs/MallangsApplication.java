8e4ff6fae2b55d08c9d12f864c10e5caecac41c9
username0w <grandcias123@gmail.com>
2024-11-27 11:51:30 +0900

    feat: MallangsApplication 어노테이션 추가

--- src/main/java/com/mallangs/MallangsApplication.java
+++ src/main/java/com/mallangs/MallangsApplication.java
@@ -2,12 +2,17 @@ package com.mallangs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA Auditing 기능 활성화
 public class MallangsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MallangsApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MallangsApplication.class, args);
+    System.out.println();
+    System.out.println(org.hibernate.Version.getVersionString());
+//    implementation group: 'org.hibernate', name: 'hibernate-spatial', version: '5.6.9.Final'
+  }
 
 }
