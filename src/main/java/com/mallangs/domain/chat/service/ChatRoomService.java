127ff1e56e046d593769c23a7dca8935c717c971
Dom1046 <rkrkwnj1046@gmail.com>
2024-11-28 16:08:56 +0900

    [feat] 채팅 서비스 crud 구현, 참여 채팅방 엔티티 EAGER -> LAZY로 수정

--- src/main/java/com/mallangs/domain/chat/service/ChatRoomService.java
+++ src/main/java/com/mallangs/domain/chat/service/ChatRoomService.java
@@ -14,10 +14,8 @@ import com.mallangs.global.exception.MallangsCustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
