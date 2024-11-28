55e4376aac87b8859166bd2bce8a9be82a9360b9
Dom1046 <rkrkwnj1046@gmail.com>
2024-11-28 16:06:57 +0900

    [feat] 채팅 메세지 레포지토리 조회 쿼리 추가

--- src/main/java/com/mallangs/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/mallangs/domain/chat/repository/ChatMessageRepository.java
@@ -1,8 +1,19 @@
 package com.mallangs.domain.chat.repository;
 
 import com.mallangs.domain.chat.entity.ChatMessage;
+import com.mallangs.domain.chat.entity.ParticipatedRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
+    @Query("SELECT DISTINCT c FROM ChatMessage c JOIN FETCH c.participatedRoom p join fetch  p.chatRoom r join fetch c.sender WHERE c.chatMessageId =:chatMessageId")
+    Optional<ChatMessage> findByChatMessageId(@Param("chatMessageId") Long chatMessageId);
+
+    @Query("SELECT DISTINCT c FROM ChatMessage c JOIN FETCH c.participatedRoom p join fetch  p.chatRoom r join fetch c.sender WHERE r.chatRoomId = :chatRoomId")
+    List<ChatMessage> findMessagesByChatRoomId(@Param("chatRoomId") Long chatRoomId);
 }
