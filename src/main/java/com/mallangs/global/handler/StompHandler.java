db6cc1f9c0fc7cae871055ffb32610867f7c4867
Dom1046 <rkrkwnj1046@gmail.com>
2024-11-28 17:16:01 +0900

    [feat] 스톰프 핸들러 구현, 토큰 검증 후 securityContext detail저장.

--- src/main/java/com/mallangs/global/handler/StompHandler.java
+++ src/main/java/com/mallangs/global/handler/StompHandler.java
@@ -1,8 +1,12 @@
 package com.mallangs.global.handler;
 
 import com.mallangs.domain.member.entity.Member;
+import com.mallangs.domain.member.entity.embadded.UserId;
+import com.mallangs.domain.member.jwt.entity.CustomMemberDetails;
 import com.mallangs.domain.member.jwt.util.JWTUtil;
 import com.mallangs.domain.member.repository.MemberRepository;
+import com.mallangs.global.exception.ErrorCode;
+import com.mallangs.global.exception.MallangsCustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.Message;
@@ -14,6 +18,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 @Log4j2
@@ -27,21 +33,25 @@ public class StompHandler implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
 
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-//
-//        //websocket 연결시
-//        if (StompCommand.CONNECT == accessor.getCommand()) {
-//            String jwtToken = accessor.getFirstNativeHeader("Authorization");
-//            if (!jwtUtil.isExpired(jwtToken)){
-//                String role = jwtUtil.getRole(jwtToken);
-//                ChatMessageDTO chatMessageDTO=(ChatMessageDTO)message.getPayload();
-//                Member foundMember = memberRepository.findById(chatMessageDTO.getMemberId()).orElseThrow(MemberException.NOT_FOUND::get);
-//                CustomUserDetails customUserDetails = new CustomUserDetails(foundMember);
-//
-//                Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,
-//                        customUserDetails.getAuthorities());
-//                SecurityContextHolder.getContext().setAuthentication(authToken);
-//            }
-//        }
+
+        //websocket 연결시
+        if (StompCommand.CONNECT == accessor.getCommand()) {
+            String jwtToken = accessor.getFirstNativeHeader("Authorization");
+            if (!jwtUtil.isExpired(jwtToken)){
+                Map<String, Object> payload = jwtUtil.validateToken(jwtToken);
+                String userId = (String)payload.get("userId");
+                Member foundMember = memberRepository.findByUserId(new UserId(userId))
+                        .orElseThrow(()->new MallangsCustomException(ErrorCode.MEMBER_NOT_FOUND));
+                CustomMemberDetails customUserDetails = new CustomMemberDetails(foundMember);
+
+                Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,
+                        customUserDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+                Object payload1 = message.getPayload();
+                log.info(" {}",payload1.toString());
+            }
+        }
         return message;
     }
 }
