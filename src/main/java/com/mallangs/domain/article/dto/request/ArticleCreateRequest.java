67cd0b6080a21da25216394ee84b9b44e431f0bb
username0w <grandcias123@gmail.com>
2024-11-29 10:51:30 +0900

    feat: 지리 데이터 필드 수정

--- src/main/java/com/mallangs/domain/article/dto/request/ArticleCreateRequest.java
+++ src/main/java/com/mallangs/domain/article/dto/request/ArticleCreateRequest.java
@@ -1,22 +1,22 @@
 package com.mallangs.domain.article.dto.request;
 
 import com.mallangs.domain.article.entity.MapVisibility;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.locationtech.jts.geom.Point;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @ToString
 @RequiredArgsConstructor
+@SuperBuilder
+@Schema(description = "부모 클래스 DTO")
 public class ArticleCreateRequest {
 
   @NotNull
   private String articleType;
-
-  @NotNull
-  private Long memberId;
 
   @NotNull
   private MapVisibility mapVisibility;
@@ -25,11 +25,12 @@ public class ArticleCreateRequest {
   private String title; // 장소인 경우 장소이름
 
   @NotNull
-  private Point geography;
+  private double latitude;
+
+  @NotNull
+  private double longitude;
 
   private String description;
-
-  private String contact;
 
   private String image;
 
