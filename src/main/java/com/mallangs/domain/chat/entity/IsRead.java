ba16e32e11de1ab60581a113dfdb56a938818c0e
Dom1046 <rkrkwnj1046@gmail.com>
2024-11-29 21:14:39 +0900

    [feat] IsRead 엔티티 추가

--- src/main/java/com/mallangs/domain/chat/entity/IsRead.java
+++ src/main/java/com/mallangs/domain/chat/entity/IsRead.java
@@ -1,19 +1,28 @@
 package com.mallangs.domain.chat.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class IsRead {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long isReadId;
 
-    private boolean read;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_message_id", nullable = false)
+    private ChatMessage chatMessage;
+
+    private String sender;
+
+    @Builder.Default
+    private boolean read = false;
+
 }
