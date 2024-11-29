fd25bf3ba47540d9566212229e032f1927864e8d
DongWooKim4343 <kd6904@naver.com>
2024-11-30 02:26:00 +0900

    [mod] API 엔드포인트 수정

--- src/main/java/com/mallangs/domain/review/controller/ReviewController.java
+++ src/main/java/com/mallangs/domain/review/controller/ReviewController.java
@@ -23,14 +23,14 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/articles/{placeArticleId}/reviews")
+@RequestMapping("/api/place-articles")
 @Tag(name = "리뷰", description = "리뷰 CRUD")
 public class ReviewController {
 
     private final ReviewService reviewService;
 
     // 리뷰 등록
-    @PostMapping
+    @PostMapping("/{placeArticleId}/reviews")
     @Operation(summary = "리뷰 등록", description = "리뷰를 등록할때 사용하는 API")
     public ResponseEntity<ReviewInfoResponse> createReview(@AuthenticationPrincipal CustomMemberDetails customMemberDetails,
                                                            @PathVariable Long placeArticleId,
@@ -40,7 +40,8 @@ public class ReviewController {
     }
 
     // 리뷰 수정
-    @PutMapping("/{reviewId}")
+    // [참고] placeArticleId는 API 경로 통일성을 위해 받지만, 실제로는 reviewId로만 식별하므로 사용하지 않음, 향후 변경가능성 고려
+    @PutMapping("/{placeArticleId}/reviews/{reviewId}")
     @Operation(summary = "리뷰 수정", description = "리뷰를 수정할때 사용하는 API")
     public ResponseEntity<ReviewInfoResponse> updateReview(@AuthenticationPrincipal CustomMemberDetails customMemberDetails,
                                                            @PathVariable Long placeArticleId,
@@ -51,7 +52,7 @@ public class ReviewController {
     }
 
     // 리뷰 삭제
-    @DeleteMapping("/{reviewId}")
+    @DeleteMapping("/{placeArticleId}/reviews/{reviewId}")
     @Operation(summary = "리뷰 삭제", description = "리뷰를 삭제하는 API (DB에서 완전삭제), 안보이게만 하고 싶은 경우 리뷰 수정 -> 숨김")
     public ResponseEntity<Void> deleteReview(@AuthenticationPrincipal CustomMemberDetails customMemberDetails,
                                              @PathVariable Long placeArticleId,
@@ -61,7 +62,7 @@ public class ReviewController {
     }
 
     // 리뷰 조회 (리뷰 ID로)
-    @GetMapping("/{reviewId}")
+    @GetMapping("/{placeArticleId}/reviews/{reviewId}")
     @Operation(summary = "리뷰 조회", description = "리뷰 ID로 리뷰를 조회하는 API.")
     public ResponseEntity<ReviewInfoResponse> getReviewById(@AuthenticationPrincipal CustomMemberDetails customMemberDetails,
                                                             @PathVariable Long placeArticleId,
@@ -71,7 +72,7 @@ public class ReviewController {
     }
 
     // 장소에 달린 리뷰 목록 조회
-    @GetMapping
+    @GetMapping("/{placeArticleId}/reviews")
     @Operation(summary = "장소 리뷰 목록 조회", description = "특정 장소에 달린 리뷰 목록을 조회하는 API.")
     public ResponseEntity<Page<ReviewInfoResponse>> getReviewsByPlaceArticleId(@PathVariable Long placeArticleId,
                                                                                PageRequest pageRequest) {
@@ -80,7 +81,7 @@ public class ReviewController {
     }
 
     // 장소에 달린 내 리뷰 조회
-    @GetMapping("/my")
+    @GetMapping("/{placeArticleId}/reviews/my")
     @Operation(summary = "특정장소의 내 리뷰 목록 조회", description = "특정 장소에 달린 내 리뷰 목록을 조회하는 API.")
     public ResponseEntity<Page<ReviewInfoResponse>> getMyReviewByPlaceArticleId(@AuthenticationPrincipal CustomMemberDetails customMemberDetails,
                                                                                 @PathVariable Long placeArticleId,
@@ -90,7 +91,7 @@ public class ReviewController {
     }
 
     // 내 리뷰 목록 조회
-    @GetMapping("/my-all")
+    @GetMapping(path = "/my-reviews")
     @Operation(summary = "내 전체 리뷰 목록 조회", description = "내가 작성한 모든 리뷰 목록을 조회하는 API.")
     public ResponseEntity<Page<ReviewInfoResponse>> getMyReviews(@AuthenticationPrincipal CustomMemberDetails customMemberDetails,
                                                                  PageRequest pageRequest) {
@@ -99,7 +100,7 @@ public class ReviewController {
     }
 
     // 특정 장소의 평균 평점 계산
-    @GetMapping("/average-score")
+    @GetMapping("/{placeArticleId}/reviews/average-score")
     @Operation(summary = "평균 평점 조회", description = "특정 장소의 평균 평점을 조회하는 API, 게시상태가 PUBLISH(공개) 상태인 것만.")
     public ResponseEntity<Double> getAverageScoreByPlaceArticleId(@PathVariable Long placeArticleId) {
         Double averageScore = reviewService.getAverageScoreByPlaceArticleId(placeArticleId);
