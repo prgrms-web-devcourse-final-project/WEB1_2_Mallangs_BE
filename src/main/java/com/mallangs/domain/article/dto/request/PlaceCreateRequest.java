6e3e98350a13492c1863a056a313cbe51ea2c7be
username0w <grandcias123@gmail.com>
2024-11-29 16:45:07 +0900

    feat: PlaceCreateRequest 수정

--- src/main/java/com/mallangs/domain/article/dto/request/PlaceCreateRequest.java
+++ src/main/java/com/mallangs/domain/article/dto/request/PlaceCreateRequest.java
@@ -1,12 +1,16 @@
 package com.mallangs.domain.article.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @ToString
 @RequiredArgsConstructor
+@SuperBuilder
+@Schema(description = "자식 클래스 DTO", allOf = ArticleCreateRequest.class)
 public class PlaceCreateRequest extends ArticleCreateRequest {
 
   private String businessHours;
@@ -14,6 +18,8 @@ public class PlaceCreateRequest extends ArticleCreateRequest {
   private String closeDays;
 
   private String website;
+
+  private String contact;
 
 
 }
