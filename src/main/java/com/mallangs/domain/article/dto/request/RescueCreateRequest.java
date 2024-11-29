4c66a16eb60db4f6dce2d5fcfd21ddd6aea9f9aa
username0w <grandcias123@gmail.com>
2024-11-29 16:31:08 +0900

    feat: RescueCreateRequest 수정

--- src/main/java/com/mallangs/domain/article/dto/request/RescueCreateRequest.java
+++ src/main/java/com/mallangs/domain/article/dto/request/RescueCreateRequest.java
@@ -1,13 +1,17 @@
 package com.mallangs.domain.article.dto.request;
 
 import com.mallangs.domain.pet.entity.PetType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @ToString
 @RequiredArgsConstructor
+@SuperBuilder
+@Schema(description = "자식 클래스 DTO")
 public class RescueCreateRequest extends ArticleCreateRequest {
 
   private PetType petType;
