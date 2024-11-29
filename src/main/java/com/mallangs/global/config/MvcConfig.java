ea91ec4751a1a6eb08b12173c34b5233b9401a60
iam52 <tupac52@gmail.com>
2024-11-29 10:19:57 +0900

    [mod] 카테고리별로 커뮤니티 게시글 조회하는 메서드 수정, MvcConfig에 ngrok 설정 추가

--- src/main/java/com/mallangs/global/config/MvcConfig.java
+++ src/main/java/com/mallangs/global/config/MvcConfig.java
@@ -7,7 +7,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000")  // 허용할 도메인
+                .allowedOrigins("http://localhost:3000", "https://*.ngrok-free.app")  // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 허용할 헤더
                 .allowCredentials(true);  // 인증 정보 허용 여부
