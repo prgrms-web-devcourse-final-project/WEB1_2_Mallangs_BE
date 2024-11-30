7dd4c1bbaf153a9e9d6018bfb1b7d0b748ba25e4
Dom1046 <rkrkwnj1046@gmail.com>
2024-12-01 01:32:00 +0900

    [refactor] 채팅 스웨거 표시 리펙토링

--- src/main/java/com/mallangs/domain/chat/controller/test/WebSocketControllerTest.java
+++ src/main/java/com/mallangs/domain/chat/controller/test/WebSocketControllerTest.java
@@ -1,6 +1,7 @@
 package com.mallangs.domain.chat.controller.test;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 @RequiredArgsConstructor
 @Log4j2
+@Tag(name = "테스트용 컨트롤러 사용 X", description = " 테스트용 사용 X")
 public class WebSocketControllerTest {
 
     //웹소켓 연결 확인 url
