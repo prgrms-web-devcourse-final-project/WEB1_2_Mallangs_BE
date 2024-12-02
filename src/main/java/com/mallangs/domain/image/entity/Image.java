7ab7104ba4a0a49e007ecf0199684e88eac7e8f4
iam52 <tupac52@gmail.com>
2024-12-03 00:23:08 +0900

    [feat] Image 엔티티 구현

--- src/main/java/com/mallangs/domain/image/entity/Image.java
+++ src/main/java/com/mallangs/domain/image/entity/Image.java
@@ -1,31 +1,63 @@
 package com.mallangs.domain.image.entity;
 
-import com.mallangs.domain.chat.entity.ChatMessage;
+import com.mallangs.domain.article.entity.Article;
+import com.mallangs.domain.board.entity.Board;
+import com.mallangs.domain.member.entity.Member;
+import com.mallangs.domain.pet.entity.Pet;
+import com.mallangs.domain.review.entity.Review;
+import com.mallangs.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Getter
-@Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "image")
-public class Image {
+@AllArgsConstructor
+@Builder
+public class Image extends BaseTimeEntity {
     @Id
-    @Column(nullable = false, unique = true)
-    private String url;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
 
+    // 이미지가 필요한 도메인: Article, Board, Member, Pet, Review
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Enumerated(EnumType.STRING)
+    private ImageOrder imageOrder;
+
+    @Column(nullable = false)
     private String originalFileName;
 
-    private String contentType;
+    @Column(nullable = false)
+    private String storedFileName;
+
+    @Column(nullable = false)
+    private int fileSize;
+
+    @Column(nullable = false)
+    private String s3Url;
 
     @Column(nullable = false)
     private Integer width;
 
     @Column(nullable = false)
     private Integer height;
-
-
-
-
 }
