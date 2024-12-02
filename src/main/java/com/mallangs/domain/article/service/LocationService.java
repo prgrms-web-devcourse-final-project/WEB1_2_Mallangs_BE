e244aec42d1067e52b7b5e8286e66e7ff69b1c69
username0w <grandcias123@gmail.com>
2024-12-03 04:07:43 +0900

    feat: 타입별 지도 조회 기능 추가

--- src/main/java/com/mallangs/domain/article/service/LocationService.java
+++ src/main/java/com/mallangs/domain/article/service/LocationService.java
@@ -14,9 +14,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LocationService {
 
-  private final EntityManager em;
   private final LocationRepository locationRepository;
-  private final ArticleRepository articleRepository;
 
   public List<MapBoundsResponse> findArticlesInBounds(double southWestLat, double southWestLon,
       double northEastLat, double northEastLon) {
@@ -27,59 +25,22 @@ public class LocationService {
     log.info(String.valueOf(northEastLat));
     log.info(String.valueOf(northEastLon));
 
-//    String polygonWkt = String.format(
-//        "POLYGON((%f %f, %f %f, %f %f, %f %f, %f %f))",
-//        southWestLat, southWestLon,
-//        southWestLat, northEastLon,
-//        northEastLat, northEastLon,
-//        northEastLat, southWestLon,
-//        southWestLat, southWestLon);
-
-//    String pointFormat = String.format("'LINESTRING(%f %f, %f %f)')", northEastLat, northEastLon,
-//        southWestLat, southWestLon);
-//    Query query = em.createNativeQuery(
-//        "SELECT a FROM article a WHERE MBRContains(ST_LINESTRINGFROMTEXT(" + pointFormat
-//            + ", r.point)", Article.class);
-//
-//    List<Article> articles = query.getResultList();
-//    return articles;
-
     return locationRepository.findArticlesInBounds(southWestLat, southWestLon, northEastLat,
         northEastLon);
   }
-//  public List<Article> findArticlesInBounds(double northEastLat, double northEastLon,
-//      double southWestLat, double southWestLon) {
-//    Coordinate[] coordinates = new Coordinate[]{
-//        new Coordinate(southWestLon, southWestLat),
-//        new Coordinate(northEastLon, southWestLat),
-//        new Coordinate(northEastLon, northEastLat),
-//        new Coordinate(southWestLon, northEastLat),
-//        new Coordinate(southWestLon, southWestLat)
-//    };
-//
-//    GeometryFactory geometryFactory = new GeometryFactory();
-//    Polygon polygon = geometryFactory.createPolygon(coordinates);
-//    polygon.setSRID(4326);
-//
-//    String polygonWkt = "SRID=4326;" + polygon.toText();
-//
-//    log.info("Find articles in bounds using polygon: " + polygonWkt);
-//
-//    return locationRepository.findArticlesInBounds(southWestLon, southWestLat, northEastLon,
-//        northEastLat);
 
-//    String polygonWkt = String.format(
-//        "SRID=4326;POLYGON((%f %f, %f %f, %f %f, %f %f, %f %f))",
-//        southWestLat, southWestLon, northEastLat, southWestLon,
-//        northEastLat, northEastLon, southWestLat, northEastLon,
-//        southWestLat, southWestLon);
-//
-//    log.info("Find articles in bounds " + polygonWkt);
-//
-//    return locationRepository.findArticlesInBounds(polygonWkt);
+  public List<MapBoundsResponse> findArticlesInBoundsByType(double southWestLat,
+      double southWestLon,
+      double northEastLat, double northEastLon, String type) {
 
-//    return locationRepository.findArticlesInBounds(northEastLat, northEastLon, southWestLat,
-//        southWestLon);
-//  }
+    log.info("findArticlesInBounds");
+    log.info(String.valueOf(southWestLat));
+    log.info(String.valueOf(southWestLon));
+    log.info(String.valueOf(northEastLat));
+    log.info(String.valueOf(northEastLon));
+
+    return locationRepository.findArticlesInBoundsByType(southWestLat, southWestLon, northEastLat,
+        northEastLon, type);
+  }
 
 }
