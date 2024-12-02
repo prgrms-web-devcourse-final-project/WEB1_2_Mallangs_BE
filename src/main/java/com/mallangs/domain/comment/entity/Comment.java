16b49863faf300a1c2262cfa9133530629b8ecf9
HanJae-Jae <gkswowo98@naver.com>
2024-12-02 11:16:59 +0900

    [feat] Comment 엔티티 작성

--- src/main/java/com/mallangs/domain/comment/entity/Comment.java
+++ src/main/java/com/mallangs/domain/comment/entity/Comment.java
@@ -1,31 +1,52 @@
 package com.mallangs.domain.comment.entity;
 
-import com.mallangs.domain.member.Member;
+import com.mallangs.domain.article.entity.Article;
+import com.mallangs.domain.board.entity.Board;
+import com.mallangs.domain.member.entity.Member;
 import com.mallangs.global.common.BaseTimeEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor (access = AccessLevel.PRIVATE)
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
 public class Comment extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="member_id")
+    @JoinColumn(name = "board_id", nullable = true)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = true)
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    @Enumerated(EnumType.STRING)
-    private PostType postType;
-
-    private Long postId;
-
+    @Column(length = 200)
     private String content;
 
     public void changeContent(String content) {
+        if (content.length() > 200) {
+            throw new IllegalArgumentException("댓글 내용은 200자 이하로 입력해야 합니다.");
+        }
         this.content = content;
     }
 }
