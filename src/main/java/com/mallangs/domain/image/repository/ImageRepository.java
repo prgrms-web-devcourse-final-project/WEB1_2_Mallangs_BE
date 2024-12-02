f9e1342b439ec66a37d4ab5222333566738f6b7d
iam52 <tupac52@gmail.com>
2024-12-03 00:58:28 +0900

    [feat] Image 레포지토리 구현, Image 관련 Global Exception 메시지 추가

--- src/main/java/com/mallangs/domain/image/repository/ImageRepository.java
+++ src/main/java/com/mallangs/domain/image/repository/ImageRepository.java
@@ -1,9 +1,46 @@
 package com.mallangs.domain.image.repository;
 
+import com.mallangs.domain.article.entity.Article;
+import com.mallangs.domain.board.entity.Board;
 import com.mallangs.domain.image.entity.Image;
+import com.mallangs.domain.member.entity.Member;
+import com.mallangs.domain.pet.entity.Pet;
+import com.mallangs.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    // Article에 연결된 이미지 조회
+    List<Image> findByArticle(Article article);
+
+    // Board에 연결된 이미지 조회
+    List<Image> findByBoard(Board board);
+
+    // Member에 연결된 이미지 조회
+    List<Image> findByMember(Member member);
+
+    // Pet에 연결된 이미지 조회
+    List<Image> findByPet(Pet pet);
+
+    // Review에 연결된 이미지 조회
+    List<Image> findByReview(Review review);
+
+    // Article에 연결된 이미지 삭제
+    void deleteByArticle(Article article);
+
+    // Board에 연결된 이미지 삭제
+    void deleteByBoard(Board board);
+
+    // Member에 연결된 이미지 삭제
+    void deleteByMember(Member member);
+
+    // Pet에 연결된 이미지 삭제
+    void deleteByPet(Pet pet);
+
+    // Review에 연결된 이미지 삭제
+    void deleteByReview(Review review);
 }
