31f53f80258828613a81bdc7a8f03ba8089e4d36
username0w <grandcias123@gmail.com>
2024-12-03 16:13:50 +0900

    feat: 위도 경도 유효성 검사 수정

--- src/main/java/com/mallangs/domain/article/dto/request/MapBoundsRequest.java
+++ src/main/java/com/mallangs/domain/article/dto/request/MapBoundsRequest.java
@@ -1,5 +1,7 @@
 package com.mallangs.domain.article.dto.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,15 +13,23 @@ import lombok.ToString;
 public class MapBoundsRequest {
 
   @NotNull(message = "북동쪽 위도 값은 필수입니다.")
+  @DecimalMin(value = "-90.0", message = "북동쪽 위도는 -90.0 이상이어야 합니다.")
+  @DecimalMax(value = "90.0", message = "북동쪽 위도는 90.0 이하이어야 합니다.")
   private Double northEastLat;
 
   @NotNull(message = "북동쪽 경도 값은 필수입니다.")
+  @DecimalMin(value = "-180.0", message = "북동쪽 경도는 -180.0 이상이어야 합니다.")
+  @DecimalMax(value = "180.0", message = "북동쪽 경도는 180.0 이하이어야 합니다.")
   private Double northEastLon;
 
   @NotNull(message = "남서쪽 위도 값은 필수입니다.")
+  @DecimalMin(value = "-90.0", message = "남서쪽 위도는 -90.0 이상이어야 합니다.")
+  @DecimalMax(value = "90.0", message = "남서쪽 위도는 90.0 이하이어야 합니다.")
   private Double southWestLat;
 
   @NotNull(message = "남서쪽 경도 값은 필수입니다.")
+  @DecimalMin(value = "-180.0", message = "남서쪽 경도는 -180.0 이상이어야 합니다.")
+  @DecimalMax(value = "180.0", message = "남서쪽 경도는 180.0 이하이어야 합니다.")
   private Double southWestLon;
 
 }
