9390b9069ed26dece789238952d629da329e1085
username0w <grandcias123@gmail.com>
2024-12-03 15:31:05 +0900

    feat: Enum 타입 유효성 검사 어노테이션 수정

--- src/main/java/com/mallangs/domain/article/dto/request/RescueCreateRequest.java
+++ src/main/java/com/mallangs/domain/article/dto/request/RescueCreateRequest.java
@@ -4,6 +4,7 @@ import com.mallangs.domain.article.entity.CaseStatus;
 import com.mallangs.domain.pet.entity.PetType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,10 @@ import lombok.experimental.SuperBuilder;
 @Schema(description = "자식 클래스 DTO", allOf = ArticleCreateRequest.class)
 public class RescueCreateRequest extends ArticleCreateRequest {
 
-  @NotBlank(message = "동물 타입은 필수입니다.")
+  @NotNull(message = "동물 타입은 필수입니다.")
   private PetType petType;
 
-  @NotBlank(message = "구조 상태는 필수입니다.")
+  @NotNull(message = "구조 상태는 필수입니다.")
   private CaseStatus rescueStatus;
 
   @NotBlank(message = "발견 장소는 필수입니다.")
