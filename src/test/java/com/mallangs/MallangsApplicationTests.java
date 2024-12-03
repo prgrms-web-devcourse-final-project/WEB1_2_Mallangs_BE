8f660c5d9ea7e79ea150939b3fca19eb42fc525b
Dom1046 <rkrkwnj1046@gmail.com>
2024-12-04 03:05:21 +0900

    새롭게 커밋

--- src/test/java/com/mallangs/MallangsApplicationTests.java
+++ src/test/java/com/mallangs/MallangsApplicationTests.java
@@ -3,9 +3,10 @@ package com.mallangs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("dev")
+@TestPropertySource(locations = "classpath:/application-dev.properties")
 class MallangsApplicationTests {
 
     @Test
