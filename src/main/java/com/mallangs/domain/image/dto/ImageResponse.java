27224ce5d4c3453a54578ff8bb723ae467c563f1
iam52 <tupac52@gmail.com>
2024-12-04 14:15:55 +0900

    [mod] 배포 빌드 에러 관련 ImageResponse 주석 처리

--- src/main/java/com/mallangs/domain/image/dto/ImageResponse.java
+++ src/main/java/com/mallangs/domain/image/dto/ImageResponse.java
@@ -4,16 +4,16 @@
 //import lombok.Getter;
 //import lombok.ToString;
 //
-////@Getter
-////@ToString
-////public class ImageResponse {
-////    private String url;
-////    private Integer width;
-////    private Integer height;
-////
-////    public ImageResponse(Image image){
-////        this.url = image.getUrl();
-////        this.width = image.getWidth();
-////        this.height = image.getHeight();
-////    }
-////}
+//@Getter
+//@ToString
+//public class ImageResponse {
+//    private String url;
+//    private Integer width;
+//    private Integer height;
+//
+//    public ImageResponse(Image image){
+//        this.url = image.getUrl();
+//        this.width = image.getWidth();
+//        this.height = image.getHeight();
+//    }
+//}
