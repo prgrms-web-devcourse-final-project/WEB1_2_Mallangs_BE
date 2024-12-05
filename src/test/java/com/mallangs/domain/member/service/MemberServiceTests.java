952373d05c370864b00375858ec7142be90125c5
Dom1046 <rkrkwnj1046@gmail.com>
2024-12-05 18:34:51 +0900

    [fix] 필터 메서드 별 필터 통과 수정

--- src/test/java/com/mallangs/domain/member/service/MemberServiceTests.java
+++ src/test/java/com/mallangs/domain/member/service/MemberServiceTests.java
@@ -1,36 +1,55 @@
 package com.mallangs.domain.member.service;
 
-import com.mallangs.domain.member.entity.Member;
-import com.mallangs.domain.member.entity.embadded.Email;
-import com.mallangs.domain.member.entity.embadded.Nickname;
-import com.mallangs.domain.member.entity.embadded.Password;
-import com.mallangs.domain.member.entity.embadded.UserId;
+import com.mallangs.domain.member.dto.MemberCreateRequest;
+import com.mallangs.domain.member.dto.MemberRegisterRequest;
+import com.mallangs.domain.member.repository.AddressRepository;
 import com.mallangs.domain.member.repository.MemberRepository;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 
-@ExtendWith(MockitoExtension.class)
-@DisplayName("[service] 회원 서비스 단위 테스트")
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-dev.properties")
 public class MemberServiceTests {
 
-    @InjectMocks
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private AddressRepository addressRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
     private MemberUserService memberUserService;
 
-    @Mock private MemberRepository memberRepository;
-    @Mock private PasswordEncoder passwordEncoder;
+    @BeforeEach
+    void setUp() {
+    }
 
-
-    @DisplayName("회원가입")
     @Test
     void createMember() {
-        //given
-        Member member = Member.builder().userId(new UserId("testUserId")).password(new Password("testPassword1!!", passwordEncoder)).email(new Email("test123@test.com")).nickname(new Nickname("testNickname")).hasPet(true).build();
 
+    }
+
+    @Test
+    void createCreateRequest() {
+        //given
+        MemberCreateRequest requestData = createData();
+
+        //when
+//        MemberRegisterRequest memberRegisterRequest = memberUserService.create(requestData);
+//
+//        then
+//        Assertions.assertEquals(memberRegisterRequest.getUserId(), "testId1223");
+    }
+
+
+    MemberCreateRequest createData() {
+        return MemberCreateRequest.builder().userId("test1234213").password("P@ss0rd1223")
+                .email("test.email2213024@provider.net").nickname("testname2").hasPet(true).build();
     }
 
 }
