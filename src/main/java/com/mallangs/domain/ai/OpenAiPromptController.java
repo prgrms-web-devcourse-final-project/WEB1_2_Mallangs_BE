af8c5c7ba6f9273db6ad4a878b1a56c249dacfc3
Dom1046 <rkrkwnj1046@gmail.com>
2025-01-05 10:08:50 +0900

    openai code 수정

--- src/main/java/com/mallangs/domain/ai/OpenAiPromptController.java
+++ src/main/java/com/mallangs/domain/ai/OpenAiPromptController.java
@@ -1,48 +1,27 @@
 package com.mallangs.domain.ai;
 
-import com.mallangs.domain.ai.dto.SightAIResponse;
-import com.openai.client.OpenAIClient;
-import com.openai.client.okhttp.OpenAIOkHttpClient;
-import com.openai.models.*;
-import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/ai")
 public class OpenAiPromptController {
 
-    @Value("${spring.ai.open.ai.secret.key}")
-    private String SECRET_KEY;
+    private final ChatClient chatClient;;
+
+    OpenAiPromptController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
 
     @GetMapping()
-    @Operation(summary = "AI로 비슷한 목격게시물을 조회", description = "AI로 실종글타래와 비슷한 목격게시물을 조회합니다.")
-    public ResponseEntity<String> getArticleByArticleIdByAI(){
-
-        //ai client를 api key를 이용해 등록
-        OpenAIClient client = OpenAIOkHttpClient.builder()
-                .apiKey(SECRET_KEY)
-                .build();
-
-        //ai 대화형응답에 대한 param을 만들기
-        String question = "Say this is a test";
-
-        ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
-                .messages(List.of(ChatCompletionMessageParam.ofChatCompletionUserMessageParam(ChatCompletionUserMessageParam.builder()
-                        .role(ChatCompletionUserMessageParam.Role.USER)
-                        .content(ChatCompletionUserMessageParam.Content.ofTextContent(question))
-                        .build())))
-                .model(ChatModel.O1)
-                .build();
-
-        //응답 생성 - validate()는 자바 역직렬화 타입 확인 OpenAIInvalidDataException 방어코드
-        ChatCompletion chatCompletion = client.chat().completions().create(params).validate();
-
-        return ResponseEntity.ok(chatCompletion.toString());
+    public String call (@RequestBody String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
     }
+
 }
