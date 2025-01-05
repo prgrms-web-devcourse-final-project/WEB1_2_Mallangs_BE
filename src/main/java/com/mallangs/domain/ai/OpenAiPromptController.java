cdd4b6235538d8204be5bb20eb57030ef61df4fb
Dom1046 <rkrkwnj1046@gmail.com>
2025-01-05 10:23:33 +0900

    chat gpt 모델명 변경

--- src/main/java/com/mallangs/domain/ai/OpenAiPromptController.java
+++ src/main/java/com/mallangs/domain/ai/OpenAiPromptController.java
@@ -1,10 +1,10 @@
 package com.mallangs.domain.ai;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/ai")
